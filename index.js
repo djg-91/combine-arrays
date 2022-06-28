504cc430337e2670bc0020199e0a14dd4ec37efa
Gregoriobart <jose.constenla@gmail.com>
2022-06-28 17:11:26 +0200

    Change array of arrays

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-let arr_1 = ['he', 'she'];
-let arr_2 = ['_'];
-let arr_3 = ['gives', 'takes', 'fucks'];
-let arr_4 = ['.'];
-let arr_5 = ['you', 'me', 'us', 'them'];
-
-arrs = [arr_1, arr_2, arr_3, arr_4, arr_5];
+arrs = [
+  ['he', 'she'],
+  ['_'],
+  ['gives', 'takes', 'challenges'],
+  ['.'],
+  ['you', 'me', 'us', 'them']
+]
 
 dummy_arr = arrs.shift();
 final_arr = [];
